91730f41080d2aaef9f996495a0f81a01f84b17f
RVleerobertdyer <ldyer@redventures.com>
2024-12-29 13:35:03 -0500

    feat: initial setup and basic style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import './App.css'
 import Hero from './Components/Hero/Hero'
 import Nav from './Components/Nav/Nav'
+import './App.css'
 
 function App() {
   return (
--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -8,6 +8,9 @@ export default function Nav() {
             <div className="navText">
                 <h1 className="navTitle">ERIn DaWn cAmPbELl</h1>
                 <h2 className="navSubTitle">Handmade Clothing & Upcycled Vintage</h2>
+                <h1 className="text-1xl font-bold underline text-slate-900">
+      Hello world!
+    </h1>
             </div>
         </div>
     )
