e330145723c3faaa2e7ba755b0ec0ff5e4de9a25
RVleerobertdyer <ldyer@redventures.com>
2025-03-15 20:35:34 -0400

    feat: add m-auto to submit button

--- src/Components/Buttons/SubmitBtn.tsx
+++ src/Components/Buttons/SubmitBtn.tsx
@@ -1,7 +1,7 @@
 
 export default function SubmitBtn({progress}: {progress: number}) {
     return (
-        <button type="submit" className="bg-edcYellow-60 text-edcPurple-80 px-4 py-2 w-[15rem] hover:bg-edcYellow-20 hover:border-2 hover:border-edcPurple-80 rounded-md disabled:bg-gray-400"
+        <button type="submit" className="m-auto bg-edcYellow-60 text-edcPurple-80 px-4 py-2 w-[15rem] hover:bg-edcYellow-20 hover:border-2 hover:border-edcPurple-80 rounded-md disabled:bg-gray-400"
         disabled={progress > 0} >
             Submit
         </button>
