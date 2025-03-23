2ef71214039414c158d2a2520a7ee265909992d6
RVleerobertdyer <ldyer@redventures.com>
2025-03-23 16:25:33 -0400

    feat: spinning gif recycle

--- src/Components/SpinningCard/SpinningCard.tsx
+++ src/Components/SpinningCard/SpinningCard.tsx
@@ -37,12 +37,22 @@ export default function SpinningCard({ photo }: { photo: IGeneralPhoto }) {
           </div>
 
           {/* Back face (Card) */}
-          <div className="absolute w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)]">
+          <div className="w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)] relative">
             <img
               src="/images/card.jpg"
               alt="Card"
               className="rounded-md h-full w-full object-cover object-center"
             />
+            {/* Centered recycling icon */}
+            <div className="absolute inset-0 w-full h-full rounded-full flex z-[20] items-start mt-[2.75rem] ml-[.25rem] bg-opacity-0 justify-center">
+              <div className="w-16 h-16 rounded-full overflow-hidden">
+                <img
+                  src="/images/recycle.gif"
+                  alt="Loading"
+                  className="w-full h-full"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
