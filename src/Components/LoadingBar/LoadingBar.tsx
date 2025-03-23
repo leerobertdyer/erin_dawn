ed00010eeaee02718ed13dab0d1d141b48b1680c
RVleerobertdyer <ldyer@redventures.com>
2025-03-23 14:53:07 -0400

    fix: loading bar update

--- src/Components/LoadingBar/LoadingBar.tsx
+++ src/Components/LoadingBar/LoadingBar.tsx
@@ -1,13 +1,9 @@
-// TODO: Update this progress bar 
+// LoadingBar component - displays progress for uploads
 export default function LoadingBar({ progress }: { progress: number }) {
    return (
-       <>
-           {progress > 0 && (
-               <div className="fixed bottom-0 w-full p-2 text-center"
-               style={{ background: `linear-gradient(to right, #4CAF50 ${progress}%, #f1f1f1 ${progress}%)` }}>
-               <p>Upload Progress: {progress}%</p>
-               </div>
-           )}
-       </>
-    )
+       <div className="w-full p-2 text-center mb-2 rounded-md z-10 border border-gray-300"
+       style={{ background: `linear-gradient(to right, #4CAF50 ${progress}%, #f1f1f1 ${progress}%)` }}>
+       <p className="font-semibold">Upload Progress: {progress}%</p>
+       </div>
+   )
 }
